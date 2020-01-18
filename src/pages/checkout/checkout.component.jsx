64e604ab0f609233bfb45a412d1d9799e4feaa19
Das <maccameron@live.ca>
2020-01-18 03:14:38 -0700

    ready for first deploy

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
 
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
 
@@ -42,6 +43,8 @@ const CheckoutPage = ({ cartItems, total }) => (
         <div className='total'>
             <span>TOTAL: ${total}</span>
         </div>
+        {/* <div className='test-warning'>Please use test credit card, this is a mockup app</div> */}
+        <StripeCheckoutButton price={total} />
     </div>
 );
 
