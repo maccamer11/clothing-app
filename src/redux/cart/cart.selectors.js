81fd116c69eb2ef3cb68199806a9dd04718e5554
Das <maccameron@live.ca>
2020-01-17 19:26:37 -0700

    added checkoutpage item component

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -15,5 +15,11 @@ export const selectCartHidden = createSelector(
 export const selectCartItemsCount = createSelector(
     [selectCartItems],
     cartItems =>
-        cartItems.reduce((accumulatedQuantity, CartItem) => accumulatedQuantity + CartItem.quantity, 0)
+        cartItems.reduce((accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity, 0)
 );
+
+export const selectCartTotal = createSelector(
+    [selectCartItems],
+    cartItems =>
+        cartItems.reduce((accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity * cartItem.price, 0)
+)
